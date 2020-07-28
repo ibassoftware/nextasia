8efefa656bd116b644ce6d757b0a0285f9f36c28
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2020-07-28 14:50:41 +0800

    Request#22

--- ibas_realestate/models/__init__.py
+++ ibas_realestate/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import models, realestate_project, sale, res_partner, hr, account
+from . import models, realestate_project, sale, res_partner, hr, account, account_payment_term
